94a05ff46fc5c973f5549a54366a742151a23e7b
Lejangt <finn-mich-gut@gmx.de>
2024-04-10 19:09:43 +0200

    All

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ready to start!
+export XAUTHORITY=~/.Xauthority
+TERM=
+sed -i -e 's/env TERM=xterm $LAUNCHER "$@"$/env SHELL=\/bin\/bash TERM=xterm xterm  -e "\/bin\/bash -c $LAUNCHER "$@""/' /data/Stardew/Stardew\ Valley/StardewValley
+
+bash -c "/data/Stardew/Stardew\ Valley/StardewValley"
